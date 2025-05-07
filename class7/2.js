164cc16410be2f86f0fbefea0ff319f9c2af8c7a
Kamireddynavyareddy <navyareddy3777@gmail.com>
2025-05-07 19:05:57 +0530

    newjs

---
+++ class7/2.js
@@ -0,0 +1,6 @@
+let eids=[101,102,103,104,105,106]
+//read array and elements using forloop,whileloop,do while loop
+//for of-loop
+for(eids of eids){
+    console.log(eids)
+}
---
+++ class7/array.js
@@ -0,0 +1,8 @@
+let eids=[101,102,103,104,105,106]
+//read array and elements
+console.log(eids[0])
+console.log(eids[1])
+console.log(eids[2])
+console.log(eids[3])
+console.log(eids[5])
+console.log(eids[6])
